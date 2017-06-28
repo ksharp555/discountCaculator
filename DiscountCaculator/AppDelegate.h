ec09d914eb499df9f4a1c56a2a02d9d6da9d8dfd
ksharp555 <jiahechang01@gmail.com>
2017-06-28 14:19:28 -0700

    Initial Commit

---
+++ DiscountCaculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiscountCaculator
+//
+//  Created by Jiahe Chang on 6/28/17.
+//  Copyright © 2017 Jiahe Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DiscountCaculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiscountCaculator
+//
+//  Created by Jiahe Chang on 6/28/17.
+//  Copyright © 2017 Jiahe Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
